2c7b7a71992c45a8aca9b00798e2ca54ba203ced
Martin Pinter <martin.pinter@vacuumlabs.com>
2022-08-31 16:32:24 +0200

    Remove build from git

--- build/src/index.js
+++
@@ -1,7 +0,0 @@
-console.log('Run required ts files directly with bun 🐰, for example:');
-console.log('');
-console.log('bun run src/city-library-v4-migration/migration.ts');
-console.log('');
-console.log('If you do not yet have bun, install with the following:');
-console.log('');
-console.log('curl https://bun.sh/install | bash');
